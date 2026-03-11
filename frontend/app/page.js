705c8ff4a70b75a28b847489386cc1e5d7c496bf
Eshaan Patel <eshaanp21@outlook.com>
2026-03-11 19:25:40 -0400

    Setting up actual frontend. Still messy

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,15 +1,56 @@
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  const games = [
+    {
+    id: 1,
+    awayTeam: "Yankees ",
+    awayScore: 4,
+    homeTeam: "Red Sox ",
+    homeScore: 5,
+    status: "Live",
+    },
+    {
+      id: 2,
+      awayTeam: "Giants ",
+      awayScore: "-",
+      homeTeam: "Dodgers ",
+      homeScore: "-",
+      status: "9:00 PM EST",
+    },
+  ]
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>I am learning!</h1>
-        <p>Will this work?help</p>
-        <section>
-          <div>
-            Game Card help
-          </div>
+        
+        <header className={styles.header}>
+          <h1>Baseball Dashboard</h1>
+          <p>Live Games</p>
+        </header>
+        
+        
+        <section className={styles.gamesSection}>
+          {games.length === 0 ? (
+            <p>No Games Today</p>
+          ):(
+            games.map((game, index) => (
+              <div key={index} className={styles.gameCard}>
+                <div className={styles.teamRow}>
+                  <span>{game.awayTeam}</span>
+                  <span>{game.awayScore}</span>
+                </div>
+                <div className={styles.teamRow}>
+                  <span>{game.homeTeam}</span>
+                  <span>{game.homeScore}</span>
+                </div>
+                <p className = {styles.gameStatus}>
+                  <span>{game.status}</span>
+                </p>
+              </div>
+            ))
+          )}
         </section>
       </main>
     </div>
